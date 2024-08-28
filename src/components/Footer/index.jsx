591f5963a421f250028f94bdde76437760813716
Micaela Marques <micaellagrangeiro@gmail.com>
2024-08-28 15:52:43 -0300

    adjusted footer and fonts of page

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,8 +3,12 @@ import { Container, ImgfootersContainer } from './styles'
 export function Footer() {
   return (
     <Container>
-      <ImgfootersContainer />
-      <p>© 2023 - Todos os direitos reservados.</p>
+      <div>
+        <ImgfootersContainer />
+      </div>
+      <div>
+        <p>© 2023 - Todos os direitos reservados.</p>
+      </div>
     </Container>
   )
 }
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,22 +1,50 @@
 import styled from 'styled-components'
 import footerimage from '../../assets/imgfooter.png'
+import {DEVICE_BREAKPOINTS} from '../../styles/deviceBreakpoints'
 
 export const Container = styled.div`
+  background-color: ${({ theme }) => theme.COLORS.DARK_DARK_600};
+
+  width: 100%;
   height: 77px;
   display: flex;
   align-items: center;
-  justify-content: space-around;
-  background-color: ${({ theme }) => theme.COLORS.DARK_DARK_600};
-  position: fixed;
   bottom: 0;
-  width: 100%;
   left: 0;
+  padding: 27px 29px;
+  position: fixed;
 
-`
+  p {
+    font-family: 'DM Sans', sans-serif;
+    font-weight: 400;
+    font-size: 9px;
+
+  }
+  @media (min-width: ${DEVICE_BREAKPOINTS.SM}) {
+    display: flex;
+    justify-content: space-around;
+    margin: 0 auto;
+
+    p {
+    font-family: 'DM Sans', sans-serif;
+    font-weight: 400;
+    font-size: 14px;
+
+  }
+  }
+   
+`;
 
 export const ImgfootersContainer = styled.div`
   background: url(${footerimage}) no-repeat;
   background-size: contain;
-  width: 196px;
+  min-width: 142px;
+  height: 18px;
+  width: 100%;
+
+  @media (min-width: ${DEVICE_BREAKPOINTS.SM}) {
+    min-width: 186px;
   height: 30px;
+  width: 100%;
+  }
 `
