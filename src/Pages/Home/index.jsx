437178fd516ecfd16ad53decd9285466d2113a2c
Micaela Marques <micaellagrangeiro@gmail.com>
2024-09-27 01:10:47 -0300

    adjusted function home

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -2,37 +2,64 @@ import { Container } from './styles'
 import { Footer } from '../../components/Footer'
 import { Header } from '../../components/Header'
 import { Catalog } from '../../components/Catalog'
-import { snackData } from './Refeições'
-import { dessertData } from './Sobremesa'
-import { drinksData } from './Bebidas'
+import { useEffect, useState, useCallback } from 'react'
+import { api } from '../../Services/api'
 
+export function Home({ userDefault, searchName }) {
+  const fetchProducts = useCallback(async () => {
+    const initialProducts = [
+      {
+        id: 1,
+        title: 'Refeições',
+        foods: [],
+        userDefault
+      },
+      {
+        id: 2,
+        title: 'Sobremesas',
+        foods: [],
+        userDefault
+      },
+      {
+        id: 3,
+        title: 'Bebidas',
+        foods: [],
+        userDefault
+      }
+    ]
 
-export function Home({ userDefault }) {
-  const listCatalogs = [
-    {
-      id: 1,
-      title: 'Refeição',
-      foods: snackData,
-      userDefault: userDefault
-    },
-    {
-      id: 2,
-      title: 'Sobremesa',
-      foods: dessertData,
-      userDefault: userDefault
-    },
-    {
-      id: 3,
-      title: 'Bebidas',
-      foods: drinksData,
-      userDefault: userDefault
+    try {
+      const updatedProducts = await Promise.all(
+        initialProducts.map(async (product) => {
+          const queryParams = new URLSearchParams({
+            category: product.title,
+            name: searchName || ''
+          }).toString()
+
+          const { data } = await api.get(`/product?${queryParams}`)
+          return { ...product, foods: data }
+        })
+      )
+
+      setProducts(updatedProducts)
+    } catch (error) {
+      console.error(
+        'Erro ao buscar produtos:',
+        error.response ? error.response.data : error.message
+      )
     }
-  ]
+  }, [searchName, userDefault]) // Incluímos userDefault também
+
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    fetchProducts()
+  }, [fetchProducts])
 
   return (
     <Container>
       <Header />
-      {listCatalogs.map((catalogItem) => (
+      {products.map((catalogItem) => (
         <Catalog
           key={catalogItem.id}
           {...catalogItem}
